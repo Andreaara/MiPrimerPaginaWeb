8c9763e7917743979a00351e770950ced42723de
Andreaara <20243ds171@utez.edu.mx>
2025-10-06 13:30:30 -0600

    listo trbaja js

---
+++ JavaScript/JS/App.js
@@ -0,0 +1,24 @@
+ var nombre;
+        let nombreUsuario;
+
+        let resultado1;
+        let resultado2;
+        let resultado3;
+        let resultado4;
+
+        resultado1 = 8+9;
+        
+        resultado2 = 10 - 8;
+
+        resultado3 = 9*9;
+
+        resultado4 = 10/10;
+
+        console.log("suma: " + resultado1 + " resta: " + resultado2 + " multi: " +  resultado3 + " division: " + resultado4);
+
+         document.getElementById(resultado1, resultado2, resultado3, resultado4);
+
+         document.write(" Sumita " + resultado1);
+         document.write(" resta " + resultado2);
+         document.write(" multi " + resultado3);
+         document.write(" divi " + resultado4);
