84f5a52c01ed1c93a5e65af8d89a72488d761805
Andrea Aragón <20243ds171@utez.edu.mx>
2025-10-09 13:50:28 -0600

    trabajo operaciones

--- JavaScript/JS/App.js
+++ JavaScript/JS/App.js
@@ -1,23 +1,58 @@
+let sumar = document.getElementById("sumar");
 
-let num = document.getElementById("num").value;
-let num2 = document.getElementById("num2").value;
-      let resultado1;
-        let resultado2;
-        let resultado3;
-        let resultado4;
+function realizarSuma(){
+  let numero1 = parseFloat(document.getElementById("numero1").value);
+  let numero2 = parseFloat(document.getElementById("numero2").value);
+  let operacionSuma = numero1 + numero2;
+  let mostrarResultado = document.getElementById("mostrarResultado");
+  mostrarResultado.innerHTML="<p>El resultado es" +operacionSuma+"</p>";
+  console.log("El resultado es"+ operacionSuma);
 
-        resultado1 = num+num2;
-        
-        resultado2 = num - num2;
+}
 
-        resultado3 = num*num2;
+sumar.addEventListener("click", realizarSuma);
 
-        resultado4 =num/num2;
 
-        console.log("suma: " + resultado1);
+let resta = document.getElementById("resta");
 
-         document.getElementById(resultado1, resultado2, resultado3, resultado4);
+function realizarResta(){
+  let numero1 = parseFloat(document.getElementById("numero1").value);
+  let numero2 = parseFloat(document.getElementById("numero2").value);
+  let operacionResta = numero1 - numero2;
+  let mostrarResultado = document.getElementById("mostrarResultado");
+  mostrarResultado.innerHTML="<p>El resultado es" +operacionResta+"</p>";
+  console.log("El resultado es"+ operacionResta);
 
-          document.getElementById("resultado1").textContent = resultado1;
+}
 
-    
+resta.addEventListener("click", realizarResta);
+
+
+let multipli =document.getElementById("multipli");
+
+function realizarMultipli(){
+  let numero1 = parseFloat(document.getElementById("numero1").value);
+  let numero2 = parseFloat(document.getElementById("numero2").value);
+  let operacionMultipli = numero1 * numero2;
+  let mostrarResultado = document.getElementById("mostrarResultado");
+  mostrarResultado.innerHTML="<p>El resultado es" +operacionMultipli+"</p>";
+  console.log("El resultado es"+ operacionMultipli);
+
+}
+
+multipli.addEventListener("click", realizarMultipli);
+
+
+let divi = document.getElementById("divi");
+function realizarDivision(){
+
+  let numero1 = parseFloat(document.getElementById("numero1").value);
+  let numero2 = parseFloat(document.getElementById("numero2").value);
+  let operaciondivi =  numero1 / numero2;
+  let mostrarResultado = document.getElementById("mostrarResultado");
+  mostrarResultado.innerHTML="<p>El resultado es" +operaciondivi+"</p>";
+  console.log("El resultado es"+ operaciondivi);
+
+}
+
+divi.addEventListener("click", realizarDivision);
