9a890ff658197ec88925fe3619654f3aea4cc986
Andrea Aragón <20243ds171@utez.edu.mx>
2025-10-13 13:46:47 -0600

    trabajo localstorage

---
+++ LocalStorage/local/app.js
@@ -0,0 +1,26 @@
+//Clase
+const btnGuardar = document.querySelector(".btnGuardar");
+const nombre = document.querySelector(".nombre");
+const apellido = document.querySelector(" .apellido");
+const fecha = document.querySelector (".fecha");
+const saludo = document.querySelector(".saludo");
+
+//Mostrar info en localstorage
+const textoGuardado = localStorage.getItem("texto");
+if(textoGuardado != ""){
+    saludo.textContent = `Guardado: ${textoGuardado}`;
+
+}
+
+
+
+
+btnGuardar.addEventListener("click", ()=>{
+    const texto = nombre.value;
+    const texto2 = apellido.value;
+    const texto3 = fecha.value;
+    if(nombre!="" && apellido!="" && fecha != null){
+        localStorage.setItem('texto', texto, texto2, texto3);
+        saludo.textContent = `Guardado: ${texto + " " + texto2 + "" +  " " +texto3}`;
+    }
+});
