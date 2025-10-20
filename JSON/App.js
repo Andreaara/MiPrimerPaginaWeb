5d999797118ac2233785114a05ad92d4f69d83a3
Andrea Aragón <20243ds171@utez.edu.mx>
2025-10-20 13:00:36 -0600

    trabjo

--- JSON/App.js
+++ JSON/App.js
@@ -1,7 +1,8 @@
 const persona = {
     nombre: "Andrea",
     edad: 20,
-    pais : "Mexico"
+    pais : "Mexico",
+    hobbies: ["Escuchar musica, dormir, ver series, estar con gatos, ver peliculas, criticar"]
 };
 
 // convertir objeto a JSON(texto)
@@ -10,3 +11,8 @@ console.log(personaJSON);
 
 const resultado = document.querySelector(".resultado");
 resultado.textContent = personaJSON;
+
+resultado.innerHTML = "Nombre: " + persona.nombre+
+"<p> Edad: " +persona.edad +
+"<p> Pais: " + persona.pais + 
+"<p> Hobbies: " +persona.hobbies
